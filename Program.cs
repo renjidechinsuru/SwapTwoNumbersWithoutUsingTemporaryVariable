67fc70366ae4c57d949795d61e8755dce8ed9676
Ai Uragaki <ai.uragaki.nz@gmail.com>
2021-06-13 08:54:33 +1200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace SwapTwoNumbersWithoutUsingTemporaryVariable
+{
+    class GFG
+    {
+        static public void Main()
+        {
+            int a = 10, b = 25;
+
+            Console.WriteLine("Value of a is : " + a);
+            Console.WriteLine("Value of b is : " + b);
+            a = a + b;
+            b = a - b;
+            a = a - b;
+            Console.WriteLine("New Value of a is : " + a);
+            Console.WriteLine("New Value of b is : " + b);
+            Console.ReadLine();
+
+
+
+
+
+        }
+    }
+}
